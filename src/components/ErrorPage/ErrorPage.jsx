4247692d4ac002038e225841eb99c48ed30505d8
unknown <shakir.evan.3070@Fec.edu.bd>
2025-09-03 19:19:14 +0600

    some nested route added

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2>Error Found</h2>
+            <link to="/">Go to Home</link>
+        </div>
+    );
+};
+
+export default ErrorPage;
